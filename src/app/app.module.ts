b4902fff78ca80e5817f5ed3fc47faccbb5af4dd
SergeyMg <sergiymig@gmail.com>
2020-04-11 19:57:59 +0300

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HomeModule } from './home/home.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
